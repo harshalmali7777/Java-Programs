484a37e6e4b4646626f2271abcb1f6061fbf9160
harshalmali7777 <harshalmali404040@gmail.com>
2025-12-11 12:19:22 +0530

    Add Pr1.java first practice program

---
+++ Day-8/Pr1.java
@@ -0,0 +1,9 @@
+public class Pr1 {
+    public static void main(String[] args) {
+        int a; //DECLARATION 
+        a=10; //INITIALIZATION
+
+        //int a=10;
+        System.out.println(a);
+    }
+}
